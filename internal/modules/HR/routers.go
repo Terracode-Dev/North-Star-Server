ad05de0ad80a6ef595ca04d51600a835f689249d
Pasiya <74621048+PasinduVithanage@users.noreply.github.com>
2025-10-11 17:33:45 +0530

    check trainer remove the authmiddleware

--- internal/modules/HR/routers.go
+++ internal/modules/HR/routers.go
@@ -50,7 +50,7 @@ func (S *HRService) registerRoutes() {
 	hrRoute.DELETE("/employee/:id", S.deleteEmployee, rba.AuthMiddelware([]string{"admin", "mod"}))
 	hrRoute.POST("/employee/login", S.employeeLogin)
 	hrRoute.PUT("/employee/empbank", S.empOnlyBankDetailsUpdate, rba.AuthMiddelware([]string{"emp"}))
-	hrRoute.POST("/checkTrainer", S.CheckIfEMPIsTrainer, rba.AuthMiddelware([]string{"admin", "mod"}))
+	hrRoute.POST("/checkTrainer", S.CheckIfEMPIsTrainer)
 	hrRoute.POST("/employee/ban", S.BanUser, rba.AuthMiddelware([]string{"admin", "mod"}))
 	// Delete employee certificates from the HR_FileSub,it table and S3
 	hrRoute.DELETE("/employee/deletefiles", S.DeleteEmployeeFiles, rba.AuthMiddelware([]string{"admin", "mod"}))
